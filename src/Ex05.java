e620bba81ef07eef08dd40938faaaf07ac7efa34
hatoritakuma <takuma.hatori@rakus-partners.co.jp>
2025-01-20 11:18:21 +0900

    second commit

---
+++ src/Ex05.java
@@ -0,0 +1,11 @@
+
+public class Ex05 {
+    public static void main(String[] args) {
+        String bigNumber = "阿僧祇(あそうぎ):那由他(なゆた):不可思議(ふかしぎ):無量大数(むりょうたいすう)";
+        
+        String[] bigNumbers = bigNumber.split(":") ;
+
+
+        System.out.println(bigNumber);
+    }
+}
